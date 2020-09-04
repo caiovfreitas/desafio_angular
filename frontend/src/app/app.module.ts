d1ea13955065ed688d0ab7e17c8ebc563a1e5108
Caio Freitas <cvaf@cin.ufpe.br>
2020-09-04 09:43:27 -0300

    Criando Footer

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -8,11 +8,13 @@ import { HeaderComponent } from './components/template/header/header.component';
 
 //Importações do Material Design
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { FooterComponent } from './components/template/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
